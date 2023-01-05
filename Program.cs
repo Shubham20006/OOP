ee77acf10d010bf47b8426493c50eaf70073e081
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-01-05 15:12:40 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryManagementDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the OOP Programs")
+        }
+    }
+}
